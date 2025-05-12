2b246fb15e24527c4e4159d5dc0e649e953a44ca
hardiksrivastavaa <hardikfgp@gmail.com>
2025-05-12 19:00:31 +0530

    update

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
   let [city, setCity] = useState("");
   let [error, setError] = useState(false);
 
-  let API_URL = "http://api.openweathermap.org/data/2.5/weather";
+  let API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
   let getCityWeather = async () => {
